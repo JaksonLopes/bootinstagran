434f940adb1a03ad3d55d2eb2ae31605d5bf4c99
jackson medeiros <jacksonlopes948@gmail.com>
2021-07-21 09:05:53 -0300

    subindos os primeiros arquivos

---
+++ teste.py
@@ -0,0 +1,11 @@
+for multcomentarios in range(1,20):
+          
+           
+    multcomentarios +=1
+    
+    comentarios = ["Eu quero"]
+    
+    lista_numerica = []
+    lista_numerica.append(str(multcomentarios))
+
+    print(comentarios + lista_numerica)
