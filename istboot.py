fb15e5a702cb2f8e7036b2eda42a62bd4a28f325
jackson <jacksonlopes948@gmail.com>
2022-01-11 23:24:13 -0300

    .

--- istboot.py
+++ istboot.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys  
 import time
 import random
 from random import randint
